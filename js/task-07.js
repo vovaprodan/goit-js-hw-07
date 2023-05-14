061b76124dd71cb273a65bfece50ea63ca532c27
vovaprodan <121820923+vovaprodan@users.noreply.github.com>
2023-05-14 12:54:56 +0200

    hrt

--- js/task-07.js
+++
@@ -1,7 +0,0 @@
-const input = document.querySelector('#font-size-control');
-const span = document.querySelector('#text');
-
-input.addEventListener('input', (event) => {
-    const fontSize = event.target.value + 'px';
-  text.style.fontSize = fontSize;
-});
